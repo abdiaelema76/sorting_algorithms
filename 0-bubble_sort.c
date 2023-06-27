12adea65d7a32f449bdb6466921c9246771a9853
Abdia Elema <gababoabdiaelema>
2023-06-27 12:11:34 +0300

    sorting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,30 +1,37 @@
 #include "sort.h"
 
-void print_array(const int *array, size_t size) {
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
     size_t i;
 
-    for (i = 0; i < size; i++) {
-        printf("%d", array[i]);
-        if (i != size - 1) {
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
             printf(", ");
-        }
+        printf("%d", array[i]);
+        ++i;
     }
     printf("\n");
 }
 
+
 void bubble_sort(int *array, size_t size) {
-    size_t i, j;
-    int temp;
-
-    for (i = 0; i < size - 1; i++) {
-        for (j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-            }
-            print_array(array, size);
-        }
+    for (c = 0 ; c < n - 1; c++)
+  {
+    for (d = 0 ; d < n - c - 1; d++)
+    {
+      if (array[d] > array[d+1]) 
+      {
+        swap       = array[d];
+        array[d]   = array[d+1];
+        array[d+1] = swap;
+      }
     }
-}
-
+  }
