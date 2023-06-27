f94a15640753f1597951a673d733b9ceffe9cab6
Abdia Elema <gababoabdiaelema>
2023-06-27 09:09:33 +0300

    sorting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,12 +5,12 @@ void bubble_sort(int *array, size_t size) {
     for (i = 0; i < size - 1; i++) {
         for (j = 0; j < size - i - 1; j++) {
             if (array[j] > array[j + 1]) {
-                // Swap elements
+                /* Swap elements */
                 int temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
 
-                // Print the array after each swap
+                /* Print the array after each swap */
                 printf("Array after swap: ");
                 for (int k = 0; k < size; k++) {
                     printf("%d ", array[k]);
