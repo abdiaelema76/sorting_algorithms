7d00ff2f1931c03354b2491715dae3fc6c4993d4
Abdia Elema <gababoabdiaelema>
2023-06-27 09:03:09 +0300

    sorting

---
+++ 0-bubble_sort.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void bubble_sort(int *array, size_t size) {
+    int i, j;
+    for (i = 0; i < size - 1; i++) {
+        for (j = 0; j < size - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                // Swap elements
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+
+                // Print the array after each swap
+                printf("Array after swap: ");
+                for (int k = 0; k < size; k++) {
+                    printf("%d ", array[k]);
+                }
+                printf("\n");
+            }
+        }
+    }
+}
+
